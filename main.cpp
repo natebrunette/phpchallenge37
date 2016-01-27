af4a16b2e4168bc444009e804fcdd3e3aac4c839
Nate Brunette <n@tebru.net>
2016-01-27 17:46:22 -0600

    Updating to accept stdin
    Also removed string pointers as memory should get allocated on the heap if the
    string is too large.
    
    Signed-off-by: Nate Brunette <n@tebru.net>

--- main.cpp
+++ main.cpp
@@ -10,15 +10,26 @@
 
 int main(int argc, char *argv[])
 {
+    std::string originalInput;
     if (2 != argc) {
-        std::cout << "Program needs 1 argument" << std::endl;
+        std::string line;
+        while (getline(std::cin, line)) {
+            if (!line.empty()) {
+                originalInput += line + " ";
+            }
+        }
+    } else {
+        originalInput = argv[1];
+    }
+
+    if (originalInput.empty()) {
+        std::cout << "Either pass in a string as an argument or through stdin" << std::endl;
 
         return 1;
     }
 
     // get input and add space to end
-    std::unique_ptr<std::string> originalInput(new std::string(argv[1]));
-    std::unique_ptr<std::string> input(new std::string(*originalInput + " "));
+    std::string input = originalInput + " ";
 
     auto buzzwordsList = (std::string) getcwd(NULL, 0) + "/resources/buzzwords.txt";
 
@@ -36,7 +47,7 @@ int main(int argc, char *argv[])
             std::move(wordCounterPointer)
     );
 
-    std::cout << buzzwordPercentCalculator.calculatePercentBuzzwordy(*input) << '%' << std::endl;
+    std::cout << buzzwordPercentCalculator.calculatePercentBuzzwordy(input) << '%' << std::endl;
 
     return 0;
 }
